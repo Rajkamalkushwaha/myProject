94e2626d9ee226717a5c37440a7a4070ead58068
Rajkamalkushwaha <36695253+Rajkamalkushwaha@users.noreply.github.com>
2018-04-25 13:09:27 +0530

    multidatepicker calendar

---
+++ calender/cal.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <link href="jquery-ui.multidatespicker.css" rel="stylesheet" />
+    <script src="jquery-ui.multidatespicker.js" type="text/javascript"></script>
+<script>
+        $( function() {
+            $("#datepicker").multiDatesPicker();
+        } );
+</script>
+</head>
+<body>
+<div class="container">
+<div class="main">
+<form action="" method="post">
+<input type="text" name="selected_date" id="datepicker"/>
+</select><input type="submit" id="submit" value="Submit">
+</form>
+</div>
+</div>
+</body>
+</html>
